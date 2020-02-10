a546f344d1ae2fd96dad867603b0e7a1fe5923ae
Guillaume Rager <g.rager@castsoftware.com>
2020-02-10 12:38:29 +0100

    Added OWASP 2013
    html to be supported, added not available and no findings

--- reportmaker.js
+++ reportmaker.js
@@ -5,6 +5,7 @@ var myArgs = process.argv.slice(2);
 var input = '';
 var output = '';
 var report = '';
+var format = 'PDF';
 
 if ( myArgs.length > 0) {
 
@@ -24,6 +25,12 @@ if ( myArgs.length > 0) {
       output = myArgs[i+1];
 
       console.log("output="+output);
+    }
+
+    if ( myArgs[i] == '--format' ) {
+      format = myArgs[i+1];
+
+      console.log("format="+format);
     }
 
     if ( myArgs[i] == '--report' ) {
@@ -62,6 +69,7 @@ try {
 }
 catch(error) {
   console.error(error);
+  process.exit(1);
 }
 
 const fonts = {
@@ -88,22 +96,13 @@ const h1 = { fontSize: 18, font: fonts.HelveticaBold };
 const h2 = { fontSize: 16, font: fonts.HelveticaBold };
 const h3 = { fontSize: 14, font: fonts.HelveticaBold };
 const h4 = { fontSize: 12, font: fonts.HelveticaBold };
-
-console.log('PDF Maker new document...');
-
-const doc = new pdf.Document({
-  font:    fonts.Helvetica,
-  padding: 10
-});
-
-doc.pipe(fs.createWriteStream(output));
+const paragraphFormat = {fontSize: 11, color: "#AAAAAA"};
 
 
-
-function generateOWASP2017Report() {
+function generateOWASPReport(reporttitle, categoriesObjects) {
 
     doc.cell(paddingBottomOptions).text('CAST Security Report',h3);
-    doc.cell(paddingBottomOptions).text('OWASP 2017 Summary Report',h4);
+    doc.cell(paddingBottomOptions).text(reporttitle,h4);
 
     var resultTags = applicationSummary["Classify by tag"];
 
@@ -118,18 +117,6 @@ function generateOWASP2017Report() {
     tr.cell('# Violations', { textAlign: 'right' })
 
     // prepare key-value mapping
-
-    var categoriesObjects = [
-    {"id":"A1-2017","name":"A1 2017 - Injection","nbviolation":"0"},
-    {"id":"A2-2017","name":"A2 2017 - Broken Authentication","nbviolation":"0"},
-    {"id":"A3-2017","name":"A3 2017 - Sensitive Data Exposure","nbviolation":"0"},
-    {"id":"A4-2017","name":"A4 2017 - XML External Entities (XXE)","nbviolation":"0"},
-    {"id":"A5-2017","name":"A5 2017 - Broken Access Control","nbviolation":"0"},
-    {"id":"A6-2017","name":"A6 2017 - Security Misconfiguration","nbviolation":"0"},
-    {"id":"A7-2017","name":"A7 2017 - Cross-Site Scripting (XSS)","nbviolation":"0"},
-    {"id":"A8-2017","name":"A8 2017 - Insecure Deserialization","nbviolation":"0"},
-    {"id":"A9-2017","name":"A9 2017 - Using Components With Known Vulnerabilities","nbviolation":"0"},
-];
 
   // Overall categories review
 
@@ -152,8 +139,14 @@ function generateOWASP2017Report() {
   Object.keys(categoriesObjects).forEach(k => {
 
     var row = table.row()
-    row.cell(categoriesObjects[k].name);
-    row.cell(categoriesObjects[k].nbviolation.toString(),{textAlign:'right'});
+    if(categoriesObjects[k]["applicable"]=="1") {
+      row.cell(categoriesObjects[k].name);
+      row.cell(categoriesObjects[k].nbviolation.toString(),{textAlign:'right'});
+    }
+    else {
+      row.cell(categoriesObjects[k].name,{color: "#CCCCCC"});
+      row.cell("N/A",{textAlign:'right',color: "#CCCCCC"});
+    }
   });
 
   // display details per owasp category
@@ -163,16 +156,23 @@ function generateOWASP2017Report() {
     doc.cell(paddingOptions).text(categoriesObjects[k].name,h4);
     var theCategoryId = categoriesObjects[k].id;
 
-    console.log(theCategoryId);
+    //console.log(theCategoryId);
+
+    var hasFindings = 0;
 
     for(j = 0; j < resultTags.length; j++) {
 
-
+      //some findings could be displayed
       if(theCategoryId == resultTags[j]["Tag Name"]) {
+        hasFindings = 1
         addDetails(resultTags[j]["Details"]);
       }
-
     }
+
+    if(hasFindings == 0) {
+      doc.text("No findings",paragraphFormat);
+    }
+
     });
 }
 
@@ -215,7 +215,13 @@ function generateBasicReport() {
       var row = table.row()
       row.cell(categoriesOutput[j]["id"].toString());
       row.cell(categoriesOutput[j]["category"]);
-      row.cell(categoriesOutput[j]["totalViolations"].toString(),{textAlign:'right'});
+
+      if(categoriesOutput[j]["applicable"]=="1") {
+        row.cell(categoriesOutput[j]["totalViolations"].toString(),{textAlign:'right'});
+      }
+      else {
+        row.cell("N/A",{textAlign:'center'});
+      }
   }
 
 
@@ -224,6 +230,9 @@ function generateBasicReport() {
     doc.pageBreak();
 
     doc.text(categoriesOutput[j]["category"]+" details",h3)
+
+    var listOfRulesArray = categoriesOutput[j]["listOfRules"];
+
 
     var cattable = doc.table({
         widths: [80, null, 200],
@@ -236,7 +245,6 @@ function generateBasicReport() {
       tr.cell('Rule Name')
       tr.cell('# Violations', { textAlign: 'right' })
 
-    var listOfRulesArray = categoriesOutput[j]["listOfRules"];
 
     for(k=0; k<listOfRulesArray.length; k++)
     {
@@ -253,68 +261,106 @@ function generateBasicReport() {
 }
 
 function addDetails(somedetails) {
-  var table = doc.table({
-            widths: [80, null, 200],
-            borderHorizontalWidths: function(i) { return i < 2 ? 1 : 0.1 },
-            padding: 5
-  })
 
-  var tr = table.header({ font: fonts.HelveticaBold, borderBottomWidth: 1.5 })
-  tr.cell('ID')
-  tr.cell('Name')
-  tr.cell('# Violations', { textAlign: 'right' })
+  if(somedetails.length>0) {
 
-  for(detailsj=0; detailsj<somedetails.length; detailsj++) {
+    var table = doc.table({
+              widths: [80, null, 200],
+              borderHorizontalWidths: function(i) { return i < 2 ? 1 : 0.1 },
+              padding: 5
+    })
 
-    var row = table.row()
-    row.cell(somedetails[detailsj]["Violation Id"].toString());
-    row.cell(somedetails[detailsj]["Violation Name"]);
-    row.cell(somedetails[detailsj]["Number of violation"].toString(),{textAlign:'right'});
+    var tr = table.header({ font: fonts.HelveticaBold, borderBottomWidth: 1.5 })
+    tr.cell('ID')
+    tr.cell('Name')
+    tr.cell('# Violations', { textAlign: 'right' })
+
+    for(detailsj=0; detailsj<somedetails.length; detailsj++) {
+
+      var row = table.row()
+      row.cell(somedetails[detailsj]["Violation Id"].toString());
+      row.cell(somedetails[detailsj]["Violation Name"]);
+      row.cell(somedetails[detailsj]["Number of violation"].toString(),{textAlign:'right'});
+    }
   }
+  else { // no findings
 
+    doc.text("No findings",paragraphFormat);
+  }
 }
 
 // start the document here
 
-// common part
-var cell = doc.cell({ paddingBottom: 0.5*pdf.cm })
+var doc;
 
-/*var header = doc.header().table({ widths: [null, null], paddingBottom: 1*pdf.cm }).row()
-header.cell().image(logo, { height: 2*pdf.cm })
-header.cell().text({ textAlign: 'right' })
-  .add('A Portable Document Format (PDF) generation library targeting both the server- and client-side.')
-  .add('https://github.com/rkusa/pdfjs', {
-    link: 'https://github.com/rkusa/pdfjs',
-    underline: true,
-    color: 0x569cd6
-  })*/
+if(format == "PDF") {
+  console.log('Report Maker create PDF document...');
+  doc = new pdf.Document({
+    font:    fonts.Helvetica,
+    padding: 10
+  });
 
-/*doc.footer()
-   .pageNumber(function(curr, total) { return curr + ' / ' + total }, { textAlign: 'center' })*/
+  doc.pipe(fs.createWriteStream(output));
 
+  doc.footer()
+     .pageNumber(function(curr, total) { return curr + ' / ' + total }, { textAlign: 'center' })
 
-cell.text(applicationSummary["Application Name"], h1)
-//cell.text.br()
-cell.text("Analysis date: "+applicationSummary["Analysis date"])
-cell.text("Total count of Files: "+applicationSummary["Total count of Files"])
+  // common part
+  const logocast = fs.readFileSync('cast.jpg');
+  const logoImgCast = new pdf.Image(logocast);
 
-if(report=="OWASP2017")
-{
-  generateOWASP2017Report();
+  var cell = doc.cell({ paddingBottom: 0.5*pdf.cm })
+  cell.image(logoImgCast, { height: 0.25*pdf.cm , align: 'right'})
+
+  cell.text(applicationSummary["Application Name"], h1)
+  //cell.text.br()
+  cell.text("Analysis date: "+applicationSummary["Analysis date"])
+  cell.text("Number of Files: "+applicationSummary["Total count of Files"])
+  cell.text("Number of Rules: "+applicationSummary["Total number of rules"])
+
+  if(report=="OWASP2017")
+  {
+    generateOWASPReport('OWASP 2017 Summary Report',[
+        {"id":"A1-2017","name":"A1 2017 - Injection","nbviolation":"0","applicable":"1"},
+        {"id":"A2-2017","name":"A2 2017 - Broken Authentication","nbviolation":"0","applicable":"1"},
+        {"id":"A3-2017","name":"A3 2017 - Sensitive Data Exposure","nbviolation":"0","applicable":"1"},
+        {"id":"A4-2017","name":"A4 2017 - XML External Entities (XXE)","nbviolation":"0","applicable":"1"},
+        {"id":"A5-2017","name":"A5 2017 - Broken Access Control","nbviolation":"0","applicable":"1"},
+        {"id":"A6-2017","name":"A6 2017 - Security Misconfiguration","nbviolation":"0","applicable":"1"},
+        {"id":"A7-2017","name":"A7 2017 - Cross-Site Scripting (XSS)","nbviolation":"0","applicable":"1"},
+        {"id":"A8-2017","name":"A8 2017 - Insecure Deserialization","nbviolation":"0","applicable":"1"},
+        {"id":"A9-2017","name":"A9 2017 - Using Components With Known Vulnerabilities","nbviolation":"0","applicable":"1"},
+        {"id":"A10-2017","name":"A10 2017 - Insufficient Logging & Monitoring","nbviolation":"0","applicable":"0"}]);
+  }
+  else if(report=="OWASP2013")
+  {
+        generateOWASPReport('OWASP 2013 Summary Report',[
+        {"id":"A1-2013","name":"A1 2013 - Injection","nbviolation":"0","applicable":"1"},
+        {"id":"A2-2013","name":"A2 2013 - Broken Authentication and Session Management","nbviolation":"0","applicable":"1"},
+        {"id":"A3-2013","name":"A3 2013 - Cross-Site Scripting (XSS)","nbviolation":"0","applicable":"1"},
+        {"id":"A4-2013","name":"A4 2013 - Insecure Direct Object References","nbviolation":"0","applicable":"1"},
+        {"id":"A5-2013","name":"A5 2013 - Security Misconfiguration","nbviolation":"0","applicable":"1"},
+        {"id":"A6-2013","name":"A6 2013 - Sensitive Data Exposure","nbviolation":"0","applicable":"1"},
+        {"id":"A7-2013","name":"A7 2013 - Missing Function Level Access Control","nbviolation":"0","applicable":"1"},
+        {"id":"A8-2013","name":"A8 2013 - Cross-Site Request Forgery (CSRF)","nbviolation":"0","applicable":"1"},
+        {"id":"A9-2013","name":"A9 2013 - Using Components With Known Vulnerabilities","nbviolation":"0","applicable":"1"},
+        {"id":"A10-2013","name":"A10 2013 - Unvalidated Redirects and Forwards","nbviolation":"0","applicable":"1"}]);
+  }
+  else {
+    generateBasicReport();
+  }
+
+  console.log('Document saved!');
+
+  doc.end();
 }
-else {
-  generateBasicReport();
+else if(format == "HTML") {
+
+  doc = fs.createWriteStream(output);
+
+  doc.once('open', function(fd) {
+  doc.write("My first row\n");
+  doc.write("My second row\n");
+  doc.end();
+});
 }
-
-// render something onto the document
-
-
-  //.add('https://github.com/rkusa/pdfjs', {
-  //  link: 'https://github.com/rkusa/pdfjs',
-//    underline: true,
-//    color: 0x569cd6
-//  })
-
-console.log('Document saved!');
-
-doc.end();
